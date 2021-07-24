dc2e319ad405c38c12a92a34ded7de777e43fa40
0Verlord-41 <kinicooo1@gmail.com>
2021-07-24 23:54:44 +0530

    Fourth update
    Project: API Tester
    - small tweaks

--- API-Tester/js/index.js
+++ API-Tester/js/index.js
@@ -31,17 +31,28 @@ customRadio.addEventListener('click', () => {
 })
 
 let addParam = document.getElementById('addParam');
-addParam.addEventListener('click', () => {
+addParam.addEventListener('click', (e) => {
+    e.preventDefault();
     let params = document.getElementById('params');
 
     let textString = `<div>
                             <label for="url">Parameter ${addParamCount + 2}</label>
                             <input type="text" id="inputkey" placeholder="Enter ${addParamCount + 2} Key">
                             <input type="text" id="inputvalue" placeholder="Enter ${addParamCount + 2} Value">
+                            <button class="deleteParam">-</button>
                         </div>`
 
     let paramElement = getElementFromString(textString);
     params.appendChild(paramElement);
+
+    let deleteParam = document.getElementsByClassName('deleteParam');
+    for(item of deleteParam){
+        item.addEventListener('click', (e)=>{
+            
+            e.target.parentElement.remove();
+        })
+    }
+
     addParamCount++;
 })
 
