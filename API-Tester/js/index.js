d53811119226d3d1ef1b8325f802c9a5e5bbd834
0Verlord-41 <kinicooo1@gmail.com>
2021-07-25 23:59:33 +0530

    Fourth update
    Project: API Tester
    - PrismJS added
    - other updates

--- API-Tester/js/index.js
+++ API-Tester/js/index.js
@@ -37,8 +37,8 @@ addParam.addEventListener('click', (e) => {
 
     let textString = `<div>
                             <label for="url">Parameter ${addParamCount + 2}</label>
-                            <input type="text" id="inputkey" placeholder="Enter ${addParamCount + 2} Key">
-                            <input type="text" id="inputvalue" placeholder="Enter ${addParamCount + 2} Value">
+                            <input type="text" id="inputkey${addParamCount + 2}" placeholder="Enter ${addParamCount + 2} Key">
+                            <input type="text" id="inputvalue${addParamCount + 2}" placeholder="Enter ${addParamCount + 2} Value">
                             <button class="deleteParam">-</button>
                         </div>`
 
@@ -46,13 +46,70 @@ addParam.addEventListener('click', (e) => {
     params.appendChild(paramElement);
 
     let deleteParam = document.getElementsByClassName('deleteParam');
-    for(item of deleteParam){
-        item.addEventListener('click', (e)=>{
-            
+    for (item of deleteParam) {
+        item.addEventListener('click', (e) => {
+
             e.target.parentElement.remove();
         })
     }
-
     addParamCount++;
 })
 
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('responsePrism').innerHTML = "Fetching Response, Please wait! .....";
+
+    let url = document.getElementById('url').value;
+    let requestType = document.querySelector("input[name='requestType']:checked").value;
+    let contentType = document.querySelector("input[name='contentType']:checked").value;
+
+    if (contentType == 'custom') {
+        data = {};
+        for (i = 0; i < addParamCount + 1; i++) {
+            if (document.getElementById('inputkey' + (i + 1)) != undefined) {
+                let key = document.getElementById('inputkey' + (i + 1)).value;
+                let value = document.getElementById('inputvalue' + (i + 1)).value;
+                data[key] = value;
+            }
+        }
+        data = JSON.stringify(data);
+    }
+    else {
+        data = document.getElementById('reqJson').value;
+    }
+
+    //for debugging
+    console.log(url);
+    console.log(requestType);
+    console.log(contentType);
+    console.log(data);
+
+    if (requestType == 'GET') {
+        fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.text())
+            .then((text) => {
+                // document.getElementById('responseJsonText').value = text;
+                document.getElementById('responsePrism').innerHTML = text;
+                Prism.highlightAll();
+            });
+    }
+
+    else {
+        fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.text())
+            .then((text) => {
+                document.getElementById('responsePrism').innerHTML = text;
+                Prism.highlightAll();
+            });
+
+    }
+});
