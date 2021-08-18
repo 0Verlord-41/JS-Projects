dca7c8ad4d28183ef235ebc860a8bee991eeaacc
0Verlord-41 <kinicooo1@gmail.com>
2021-08-18 23:59:58 +0530

    feat: enhancement
    Carousel
    - automatic carousel

--- Carousel/js/index.js
+++ Carousel/js/index.js
@@ -1,6 +1,8 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+showSlidesauto();
+
 // Next/previous controls
 function slideit(n) {
   showSlides(slideIndex += n);
@@ -25,4 +27,21 @@ function showSlides(n) {
   }
   slide[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function showSlidesauto() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlidesauto, 10000); // Change image every 2 seconds
 }
